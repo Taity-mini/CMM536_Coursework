23b742631e9badc24e53d4bbc247247e2853a580
Taity-mini <1504693@rgu.ac.uk>
2018-04-26 22:38:39 +0100

    Checkpoint *Before using plot package

--- text_Classification.r
+++ text_Classification.r
@@ -25,7 +25,13 @@ nrow(leaveTweets)
 
 #Custom Corpus Function for preprocessing  
 
-buildCorpus <- function (tweets, wholeDataSet, wordAssocation){
+buildCorpus <- function (tweets, wordCloud, wholeDataSet, wordAssocation){
+  
+  #Check the optional parameters are set
+  wordA <-hasArg(wordAssocation)
+  wds <- hasArg(wholeDataSet)
+  wordc <-  hasArg(wordCloud)
+  
   corp <- Corpus(VectorSource(tweets$text))
   
   
@@ -33,10 +39,10 @@ buildCorpus <- function (tweets, wholeDataSet, wordAssocation){
                  content_transformer(function(x) iconv(x, to='ASCII',
                                                        sub='byte')))
   # remove stop words and other preprocessing
- 
+  
   corp <- tm_map(corp, content_transformer(tolower))
   corp <- tm_map(corp, removeNumbers)
-
+  
   toSpace = content_transformer( function(x, pattern) gsub(pattern," ",x) )
   
   
@@ -51,22 +57,31 @@ buildCorpus <- function (tweets, wholeDataSet, wordAssocation){
   corp <-tm_map(corp, toSpace, "httpsw+")
   corp <-tm_map(corp, toSpace, "http:\\w+")
   corp <-tm_map(corp, toSpace, "https:\\w+")
-
+  
   corp <-tm_map(corp, toSpace, "[ \t]{2,}")
   corp <-tm_map(corp, toSpace, "^\\s+|\\s+$")
   
-
+  
   # ##Remove obvious words /stopwords
-  if(missing(wholeDataSet)){
-    corp <- tm_map(corp, function(x)removeWords(x,c(stopwords("english"),"amp", "will", "‰Û_", "https", "http", "httpsdb")))
-  } else if(wholeDataSet == TRUE){
-    corp <- tm_map(corp, function(x)removeWords(x,c(stopwords("english"),"amp", "will", "‰Û_", "https", "http", "httpsdb", "eu", "brexit", "rt", "leave", "remain", "vote")))
+  if(wds){
+    if(wholeDataSet){
+      corp <- tm_map(corp, function(x)removeWords(x,c(stopwords("english"),"amp", "will", "‰Û_", "https", "http", "httpsdb", "eu", "brexit", "rt", "leave", "remain", "vote")))
+    }
+  } else{
+    corp <- tm_map(corp, function(x)removeWords(x,c(stopwords("english"),"amp", "will", "‰Û_", "https", "http", "httpsdb")))  
   }
-
+  
+  
+  
   #remove punctuation last so urls are removed correctly
   corp <- tm_map(corp, removePunctuation)
   
-  if(missing(wordAssocation)){
+  #If wordAssocation is true then create document term matrix
+  if(wordA){
+    if(wordAssocation){
+      tdm <- TermDocumentMatrix(corp)
+    }
+  }else if(!wordA){ # If no  wordAssocation then create TermDocument Matrix
     tdm <- TermDocumentMatrix(corp)
     
     m <- as.matrix(tdm)
@@ -74,16 +89,28 @@ buildCorpus <- function (tweets, wholeDataSet, wordAssocation){
     d <- data.frame(word = names(v), freq = v)
     d$word <- gsub("˜", " ", d$word) ## Edit 2
     
-    tweets <- d$word
-  }else if(wordAssocation == TRUE){
-    dtm <- DocumentTermMatrix(corp)
+    
+    if(wordc){
+      if(wds){
+        tweets <- d$word
+      } else{
+        tweets <- d$word
+        if(wordCloud){
+          wordcloud(words =tweets, freq = d$freq, min.freq = 3,
+                    max.words=2000, random.order=FALSE, rot.per=0.2,
+                    colors=brewer.pal(8, "Dark2"))
+        }
+      }
+      
+    }
+    
   }
+  
 }
 
+leaveWordCloud <- buildCorpus(leaveTweets,TRUE)
 
-leaveWordCloud <- buildCorpus(leaveTweets)
-
-remainWordCloud <- buildCorpus(remainTweets)
+remainWordCloud <- buildCorpus(remainTweets,TRUE)
 
 
 wordcloud(words = leaveWordCloud, freq = d$freq, min.freq = 3,
@@ -100,6 +127,9 @@ wordcloud(words = remainWordCloud, freq = d$freq, min.freq = 3,
 
 # 1) Find the most frequent word in the collection of tweets
 
+tweetWordCloud <-buildCorpus(leaveRemainTweets, FALSE, TRUE)
+head(tweetWordCloud,1)
+
 tweetWordCloud <-buildCorpus(leaveRemainTweets, TRUE)
 
 head(tweetWordCloud,1)
@@ -109,10 +139,17 @@ head(tweetWordCloud,1)
 wordAssoication <- buildCorpus(leaveRemainTweets,TRUE,TRUE)
 
 
-(freq.terms <- findFreqTerms(wordAssoication, lowfreq = 15))
+freq.terms <- findFreqTerms(wordAssoication, lowfreq = 15)
 
 findAssocs(wordAssoication, freq.terms, 0.2)
 
+
+
+wordAssoication <- buildCorpus(leaveRemainTweets,FALSE,TRUE,TRUE)
+freq.terms <- findFreqTerms(wordAssoication, lowfreq =50)
+
+
+plot(wordAssoication, term = freq.terms, corThreshold = 0.12, weighting = T)
 
 # cor_1 <- findAssocs(wordAssoication, colnames(wordAssoication)[1:2], 0))
 (cor_1 <- findAssocs(wordAssoication, head(tweetWordCloud,10), 0))
