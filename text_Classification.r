921637094cf7c667850592aa110b2e5adaf523c6
Taity-mini <1504693@rgu.ac.uk>
2018-04-27 01:26:42 +0100

    Text Classification nearly done *SVM model works *Will need to replace RF model *Graveyard shift part 2

--- text_Classification.r
+++ text_Classification.r
@@ -25,12 +25,13 @@ nrow(leaveTweets)
 
 #Custom Corpus Function for preprocessing  
 
-buildCorpus <- function (tweets, wordCloud, wholeDataSet, wordAssocation){
+buildCorpus <- function (tweets, wordCloud, wholeDataSet, wordAssocation, dtm){
   
   #Check the optional parameters are set
   wordA <-hasArg(wordAssocation)
   wds <- hasArg(wholeDataSet)
   wordc <-  hasArg(wordCloud)
+  dtmcheck <- hasArg(dtm)
   
   corp <- Corpus(VectorSource(tweets$text))
   
@@ -76,12 +77,24 @@ buildCorpus <- function (tweets, wordCloud, wholeDataSet, wordAssocation){
   #remove punctuation last so urls are removed correctly
   corp <- tm_map(corp, removePunctuation)
   
-  #If wordAssocation is true then create document term matrix
+  #If wordAssocation is true then 
+  
+
+  
+  #If wordAssocation is true then create term document  matrix
   if(wordA){
     if(wordAssocation){
       tdm <- TermDocumentMatrix(corp)
     }
-  }else if(!wordA){ # If no  wordAssocation then create TermDocument Matrix
+    else{
+      if(dtm){
+        dtm <- DocumentTermMatrix(corp)
+      }
+    }
+    
+  }
+ 
+  else if(!wordA){ # If no  wordAssocation then create TermDocument Matrix
     tdm <- TermDocumentMatrix(corp)
     
     m <- as.matrix(tdm)
@@ -105,7 +118,7 @@ buildCorpus <- function (tweets, wordCloud, wholeDataSet, wordAssocation){
     }
     
   }
-  
+ 
 }
 
 leaveWordCloud <- buildCorpus(leaveTweets,TRUE)
@@ -148,11 +161,14 @@ findAssocs(wordAssoication, freq.terms, 0.2)
 wordAssoication <- buildCorpus(leaveRemainTweets,FALSE,TRUE,TRUE)
 freq.terms <- findFreqTerms(wordAssoication, lowfreq =50)
 
+library("Rgraphviz")
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("Rgraphviz")
 
 plot(wordAssoication, term = freq.terms, corThreshold = 0.12, weighting = T)
 
-# cor_1 <- findAssocs(wordAssoication, colnames(wordAssoication)[1:2], 0))
-(cor_1 <- findAssocs(wordAssoication, head(tweetWordCloud,10), 0))
+
 
 
 # 3) Top Most Frequent Words in Both Leave and Remain tweets
@@ -167,9 +183,69 @@ remainWords <- buildCorpus(remainTweets, TRUE)
 head(remainWords, 10)
 
 
+#Text classification
+
+#Get term document matrix
+dtm <-  buildCorpus(leaveRemainTweets,FALSE,TRUE,FALSE,TRUE)
+
+dtm <- as.matrix(dtm)
+tweets <- data.frame(dtm)
+
+tweets$label <- leaveRemainTweets$label
+
+#Split data into training and testing subsets
+#Divide the dataset into 70% training and 30% testing, to prevent overfitting from occurring
+inTrainTweets <- createDataPartition(y=tweets$label, p=0.7, list=FALSE)
+
+#Assign indexes to split the Tweets DTM into training and testing
+training <- tweets[inTrainTweets,]
+testing <- tweets[-inTrainTweets,]
+
+
+cl <- makeCluster(detectCores(), type='PSOCK')
+registerDoParallel(cl)
+
+
+train_control<- trainControl(method="cv", number=5,verboseIter=FALSE)
 
 
 
+svmModel <- train(label~ ., data = training,
+                  trControl = train_control,
+                  tuneLength =10,
+                  method = "svmRadial"
+                
+)
+
+svmModel
+
+#Evaluate
+
+predictkNN <- predict(svmModel,testing)
+confusionMatrix(predictkNN, testing$label)
+
+
+#Improved
+
+train_control<- trainControl(method="cv", number=10,verboseIter=FALSE)
 
 
 
+rfModel <- train(label~ ., data = training,
+                  trControl = train_control,
+                  tuneLength =10,
+                  method = "rf"
+                  
+)
+
+RFModel<- train(label~., data=training,
+                trControl=train_control,
+                method="rf",
+                tuneLength =10,
+                metric = 'Accuracy'
+)
+
+
+
+predictSVM <- predict(svmModel2,testing)
+confusionMatrix(predictSVM, testing$label)
